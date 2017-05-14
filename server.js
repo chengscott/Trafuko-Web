1a3c9f42339cbdfe6499b51dbeb98dff7dd32358
chengscott <60510scott@gmail.com>
2017-05-14 18:22:36 +0800

    server config

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+const distPath = path.join(__dirname, 'dist/', 'index.html');
+const requestLogger = require('./server/src/middleware/request-logger.js');
+const errorHandler = require('./server/src/middleware/error-handler.js');
+
+const app = express();
+
+app.use(requestLogger);
+app.use(express.static(__dirname + 'dist/'));
+app.use(express.static('dist', {
+    setHeaders: (res, path, stat) => {
+        res.set('Cache-Control', 'public, s-maxage=86400');
+    }
+}));
+app.get('*', function(req, res) {
+    res.sendFile(distPath);
+});
+app.use(errorHandler);
+
+const port = 80;
+app.listen(port, () => {
+    console.log(`Server is up and running on port ${port}...`);
+});
--- server/src/server.js
+++ server/src/server.js
@@ -3,23 +3,23 @@ const path = require('path');
 const distPath = path.join(__dirname, '../../dist/', 'index.html');
 console.log(distPath);
 const postRouter = require('./routers/post.js');
-const requestLogger = require('./middleware/request-logger.js'); 
+const requestLogger = require('./middleware/request-logger.js');
 const errorHandler = require('./middleware/error-handler.js');
-const accessControl = require('./middleware/access-controller.js'); // develop only 
+const accessControl = require('./middleware/access-controller.js'); // develop only
 
 const app = express();
 
 app.use(requestLogger);
 app.use(express.static(__dirname + '../../dist/'));
-// app.use(express.static('dist', {
-//     setHeaders: (res, path, stat) => {
-//         res.set('Cache-Control', 'public, s-maxage=86400');
-//     }
-// }));
+app.use(express.static('dist', {
+    setHeaders: (res, path, stat) => {
+        res.set('Cache-Control', 'public, s-maxage=86400');
+    }
+}));
 app.get('*', function(req, res) {
     res.sendFile(distPath);
 });
-app.use('/api', accessControl); // develop only 
+app.use('/api', accessControl); // develop only
 app.use('/api', postRouter);
 app.use(errorHandler);
 
